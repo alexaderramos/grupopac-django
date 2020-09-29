c989e2e99df3ecbc0b6f7f9bf09778ef544d2c56
Alexander R <tcristian_15.go@hotmail.com>
2020-09-29 15:52:47 -0500

    add message column

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Client(models.Model):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
+    message = models.TextField()
 
     def __str__(self):
         return f'Cliente: {self.id} {self.name}, {self.email}'

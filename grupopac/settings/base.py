7cdb5d672c361776337f717559eade4544002482
Alexander R <tcristian_15.go@hotmail.com>
2020-09-30 17:47:42 -0500

    KEROKU BASTABASE

--- grupopac/settings/base.py
+++ grupopac/settings/base.py
@@ -16,6 +16,7 @@ from pathlib import Path
 from django.urls import reverse_lazy
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -24,10 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'cuv$7jr2ygp368+fgml*=9y+j-x&69jb-s#21))f5q_h-=r27%'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
-ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -58,7 +56,7 @@ ROOT_URLCONF = 'grupopac.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': ['templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -74,20 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'grupopac.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'grupopac',
-        'USER': 'postgres',
-        'PASSWORD': 'secret',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
-    }
-}
 
 
 # Password validation

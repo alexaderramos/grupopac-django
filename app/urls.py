4d042931639f41d8372c2b8f8fc24a2de1c5b83c
Alexander R <tcristian_15.go@hotmail.com>
2020-10-01 12:42:29 -0500

    proteccion de rutas

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from app.views import create_client, index_client, store_client
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('', index_client, name="clients.index"),
-    path('registrar/', create_client, name="clients.create"),
-    path('guardar/', store_client, name="clients.store")
+    path('', login_required(index_client), name="clients.index"),
+    path('registrar/', login_required(create_client), name="clients.create"),
+    path('guardar/', login_required(store_client), name="clients.store")
 ]
--- grupopac/wsgi.py
+++ grupopac/wsgi.py
@@ -12,6 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grupopac.settings.production')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grupopac.settings.local')
 
 # application = get_wsgi_application()
 
